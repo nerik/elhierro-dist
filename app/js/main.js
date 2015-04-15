6545dd121272a4c326325578b7dcf49dbc7ed811
nerik <erik.escoffier@gmail.com>
2015-04-15 10:46:50 +0200

    some progress on the basemap

--- app/js/main.js
+++ app/js/main.js
@@ -13,6 +13,11 @@ var map = L.map('map', {
 });
 
 L.tileLayer('https://{s}.tiles.mapbox.com/v3/{key}/{z}/{x}/{y}.png', {
+	key: 'lrqdo.2f512fdf',
+	attribution: ''
+}).addTo(map);
+
+L.tileLayer('https://{s}.tiles.mapbox.com/v3/{key}/{z}/{x}/{y}.png', {
 	key: 'lrqdo.0c289a18',
 	attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
